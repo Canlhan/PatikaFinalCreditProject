21f1bf6b2c7098d17dcedc2dbcb3c95f81c87021
ilhan <canilhn1@gmail.com>
2023-02-22 16:29:59 +0300

    Initial commit

---
+++ src/main/java/com/can/PatikaFinalCreditProject/PatikaFinalCreditProjectApplication.java
@@ -0,0 +1,13 @@
+package com.can.PatikaFinalCreditProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaFinalCreditProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaFinalCreditProjectApplication.class, args);
+	}
+
+}
